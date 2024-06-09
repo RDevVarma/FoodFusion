1cce3a39898943eeba81bb101b543d503daafb09
RDevVarma <159523309+RDevVarma@users.noreply.github.com>
2024-06-09 18:04:00 +0530

    Add files via upload

---
+++ submit.php
@@ -0,0 +1,30 @@
+<?php
+$servername = "localhost:3306";
+$username = "root";
+$password = "1234";
+$dbname = "flavour_fusion";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+   
+    $name = $conn->real_escape_string($_POST['name']);
+    $email = $conn->real_escape_string($_POST['email']);
+    $message = $conn->real_escape_string($_POST['message']);
+
+   
+    $sql = "INSERT INTO contact_messages (name, email, message) VALUES ('$name', '$email', '$message')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "Message sent successfully!";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+}
+
+$conn->close();
+?>
